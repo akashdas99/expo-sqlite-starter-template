0f04a153720f7ad17d9d70338c160fe5a90da3bf
Akash Das <akashdas0599@gmail.com>
2025-11-25 17:44:04 +0530

    feat: Implement Drizzle ORM with PostgreSQL connection and schema configuration, replacing the previous SQLite setup.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,22 @@
-import { Config, defineConfig } from "drizzle-kit";
+// import { Config, defineConfig } from "drizzle-kit";
 
-export default defineConfig({
-  dialect: "sqlite",
-  driver: "expo",
-  schema: "./db/schema.ts",
-  out: "./drizzle",
-}) satisfies Config;
+// export default defineConfig({
+//   dialect: "sqlite",
+//   driver: "expo",
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+// }) satisfies Config;
+
+// import { config } from "dotenv";
+// import { defineConfig } from "drizzle-kit";
+
+// config({ path: ".env" });
+
+// export default defineConfig({
+//   schema: "./db/schema.ts",
+//   out: "./migrations",
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URI!,
+//   },
+// });
