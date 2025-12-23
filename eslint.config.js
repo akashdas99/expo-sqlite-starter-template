e6d95ef4dd2a69e44564d2da51881af4d2bef684
Akash Das <akashdas0599@gmail.com>
2025-12-23 18:13:31 +0530

    feat: Add testing support with Jest and Testing Library, update ESLint configuration for test files

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,16 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const testingLibrary = require("eslint-plugin-testing-library");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
+  },
+  {
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    ...testingLibrary.configs["flat/react"],
   },
 ]);
